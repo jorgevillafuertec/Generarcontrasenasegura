6b5f8ecba25fb3e80a9d058cf8a9febab76f76c1
jorgevillafuertec <149550040+jorgevillafuertec@users.noreply.github.com>
2023-11-21 22:00:51 -0500

    mi primir commit

---
+++ Clase1.py
@@ -0,0 +1,14 @@
+a= "Bienvenido a mi 'primer' programa"
+b= 'En mi lenguaje de programacion de "Python"'
+
+print (a+" "+b)
+
+b= "Con el uso de Python"
+
+print (a,b,"Gracisas")
+
+a= 3
+b = 5
+c= 7
+
+print (f"El numero {a} es el primero valor impar ")
