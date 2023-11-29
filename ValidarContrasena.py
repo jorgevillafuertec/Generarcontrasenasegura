6e8b87727d85164b84045854247dbe7d5685a489
jorgevillafuertec <149550040+jorgevillafuertec@users.noreply.github.com>
2023-11-28 23:07:55 -0500

    28/11/2023

---
+++ ValidarContrasena.py
@@ -0,0 +1,8 @@
+passguardada = "Maite"
+pasingresada = str(input("Ingresar contraseña: "))
+
+if passguardada.lower() == pasingresada.lower():
+   print("Son iguales")
+else:
+   print("Son diferentes")
+  
