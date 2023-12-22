f94980bc722d10b0eb55501c7efddf71e64b8462
jorgevillafuertec <149550040+jorgevillafuertec@users.noreply.github.com>
2023-12-22 05:29:00 -0500

    CambioFinal

--- Codificacion.py
+++ Codificacion.py
@@ -9,16 +9,16 @@ import random
 import string
 
 ##función principal que coordina todo el proceso##
-def generar_contrasena():
+def generar_contrasena():                                          
     
     ##Solicita al usuario la longitud deseada para la contraseña#
     longitud = validar_longitud()
     
     ##Solicita si quiere incluir mayúsculas, minúsculas, números y símbolos##
-    incluir_mayusculas = ingresar("¿Incluir mayúsculas? (Sí = 1, No = 0): ")
-    incluir_minusculas = ingresar("¿Incluir minúsculas? (Sí = 1, No = 0): ")
-    incluir_numeros = ingresar("¿Incluir números? (Sí = 1, No = 0): ")
-    incluir_simbolos = ingresar("¿Incluir símbolos? (Sí = 1, No = 0): ")
+    incluir_mayusculas = ingresar("¿Incluir mayúsculas? (Sí = 1, No = 0): ")    #llamdo a la funcion Ingresar para preguntar al usuario
+    incluir_minusculas = ingresar("¿Incluir minúsculas? (Sí = 1, No = 0): ")    #llamdo a la funcion Ingresar para preguntar al usuario
+    incluir_numeros = ingresar("¿Incluir números? (Sí = 1, No = 0): ")          #llamdo a la funcion Ingresar para preguntar al usuario
+    incluir_simbolos = ingresar("¿Incluir símbolos? (Sí = 1, No = 0): ")        #llamdo a la funcion Ingresar para preguntar al usuario
    
     ##Aqui toman las preferencias Ingresadas por el usuario##
     contrasena = contrasena_aleatoria(longitud, incluir_mayusculas, incluir_minusculas, incluir_numeros, incluir_simbolos)
@@ -41,16 +41,16 @@ def encriptar_contrasena(contrasena, clave):
     return contrasena_encriptada
 
 ##Validacion de la longitud que sea de minimo 8 y maximo 20##
-def validar_longitud():
-    while True:
-        try:
-            longitud = int(input("Ingrese la longitud de la contraseña (entre 8 y 20 caracteres): "))
-            if 8 <= longitud <= 20:
-                return longitud
-            else:
-                print("La longitud ingresada no es válida.")
-        except ValueError:
-            print("Por favor, ingrese un número válido.")
+def validar_longitud():                                                 #Funcion que se encarga de obtener y validar la longitud 
+    while True:                                                         #El bucle while True garantiza que la función siga ejecutándose hasta que se cumpla la condición para salir del bucle.
+        try:                                                            #Se incia el bloque Try para manejar posibles errores   
+            longitud = int(input("Ingrese la longitud de la contraseña (entre 8 y 20 caracteres): "))   #Input para pedir al usuario que ingrese la longitud
+            if 8 <= longitud <= 20:                                     #Verificar si es longitud esta dentro del rando
+                return longitud                                         #Si es valido retorna la longitud valida indicada  
+            else:                                                       #Si no es valido el valor indicado
+                print("La longitud ingresada no es válida.")            #Imprime el mensaje
+        except ValueError:                                              #El bloque Except maneja esta excepcion 
+            print("Por favor, ingrese un número válido.")               #Imprime que ingrese un numero valido
 
 ##Mensaje al solicitar la preferencia de la contraseña##
 def ingresar(mensaje):
@@ -63,16 +63,16 @@ def ingresar(mensaje):
 
 ##Genera la contraseña aleatorea utilizando la liberias string y random##
 def contrasena_aleatoria(longitud, incluir_mayusculas, incluir_minusculas, incluir_numeros, incluir_simbolos):
-    caracteres = ''
-    if incluir_mayusculas:
-        caracteres += string.ascii_uppercase
-    if incluir_minusculas:
-        caracteres += string.ascii_lowercase
-    if incluir_numeros:
-        caracteres += string.digits
-    if incluir_simbolos:
-        caracteres += string.punctuation
-    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
-    return contrasena
+    caracteres = ''                                                     #Crea una cadena vacía caracteres 
+    if incluir_mayusculas:                                              #Si incluir_mayusculas es verdadero
+        caracteres += string.ascii_uppercase                            #Agrega todas las letras mayúsculas de la libreria string.ascii_uppercase
+    if incluir_minusculas:                                              #Si incluir_minusculas es verdadero
+        caracteres += string.ascii_lowercase                            #Agregar todas las letras minusculade la liberia string.ascii_lowercase
+    if incluir_numeros:                                                 #Si incluir_numeros es verdadero
+        caracteres += string.digits                                     #Agregar todas los numero de la liberia string.digits
+    if incluir_simbolos:                                                #Si incluir_simbolos es verdadero
+        caracteres += string.punctuation                                #Agrega todos los símbolos de puntuación (string.punctuation
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))    #Utiliza una comprensión de lista y random.choice adicioanl genera una comtraseña aleatoria deacuerdo a la longitud solicitada 
+    return contrasena                                                   #Retorna una contraseña generada 
 ##Se genera la contraseña solicitada##
 generar_contrasena()
